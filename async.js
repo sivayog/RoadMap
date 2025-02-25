c1fc133c11f08871ba722255b57792d20c38e83d
siva sundaram <31383951+sivayog@users.noreply.github.com>
2025-02-25 20:51:24 +0530

    Add files via upload

---
+++ async.js
@@ -0,0 +1,129 @@
+// async function getdata() {
+//     return 'startupguy'
+    
+// }
+// const data = getdata().then((res)=>{
+//     console.log('res',res)
+// })
+// console.log('data',data)
+
+// const p = new Promise((resolve,reject)=>{
+// resolve('dsdssfsffd')
+// })
+// async function getdata() {
+//     return p
+    
+// }
+// const data = getdata().then((res)=>{
+//     console.log('res',res)
+// })
+// console.log('data',data)
+
+// const p = new Promise((resolve,reject)=>{
+// resolve('dsdssfsffd')
+// })
+
+// function getdata(){
+//     p.then((res)=>{
+//         console.log(res)
+//     })
+// }
+// getdata()
+
+// const p = new Promise((resolve,reject)=>{
+// resolve('dsdssfsffd')
+// })
+
+// async function getdata() {
+//     const val = await p
+//     console.log(val)
+    
+// }
+// getdata()
+
+// const p = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('startupguy')
+//     },8000)
+// })
+
+// function getdata() {
+// console.log('fdsfsfsfs')
+//      p.then(res=>{console.log(res)})
+    
+// }
+// getdata()
+
+
+// const p = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('dsdssfsffd')
+//     },10000)
+
+// })
+
+// async function getdata() {
+//     const val = await p
+//     console.log('startupguy')
+//     console.log(val)
+
+//     const val1 = await p
+//     console.log('startupguy1')
+//     console.log(val1)
+    
+// }
+// getdata()
+
+
+// const p = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('dsdssfsffd')
+//     },10000)
+
+// })
+
+// const p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('dsdssfsffd')
+//     },5000)
+
+// })
+
+// async function getdata() {
+//     const val = await p
+//     console.log('startupguy')
+//     console.log(val)
+
+//     const val1 = await p1
+//     console.log('startupguy1')
+//     console.log(val1)
+    
+// }
+// getdata()
+
+
+// const p = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('gfddd')
+//     },5000)
+
+// })
+
+// const p1 = new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve('dsdssfsffd')
+//     },10000)
+
+// })
+
+// async function getdata() {
+//     const val = await p
+//     console.log('startupguy')
+//     console.log(val)
+
+//     const val1 = await p1
+//     console.log('startupguy1')
+//     console.log(val1)
+    
+// }
+// getdata()
